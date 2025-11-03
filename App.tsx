9a1513e30db8da4a58a4cf2a1ef55cf7d4c63695
msotkasi20 <msotkasi20@gmail.com>
2025-11-03 15:05:40 +0200

    Second commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [sek, setSek] = useState<string>("")
+  const sekAsNumber: number = !isNaN(Number(sek))===true ? Number(sek) : 0
+  const eur: number = sekAsNumber * 0.091
+
   return (
     <View style={styles.container}>
-      <Text>LiirunLaarum</Text>
+      <Text style={styles.heading}>SEK to EUR</Text>
+      <TextInput 
+        placeholder='Enter SEK'
+        keyboardType='number-pad' 
+        style={styles.field}
+        value={sek}
+        onChangeText={setSek}
+        />
+      <Text style={styles.field}>{eur.toFixed(2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +30,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize: 24
+  },
+  field: {
+    marginVertical: 8
+  }
 });
